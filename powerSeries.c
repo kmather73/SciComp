f2fc141fe039eb10e8654708cb1c8b97dfc61f74
kmather73 <kmather73@users.noreply.github.com>
2015-10-26 21:36:24 -0500

    Update powerSeries.c

--- powerSeries.c
+++ powerSeries.c
@@ -2,29 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <cmath>
-double powerSeries(double, const double, int*);
+double powerSeries(double, const double, int&);
 
 
 main(){
 	const double epsilon = std::pow(10, -10);
-	int* iterations = (int*) malloc(sizeof(int));
+	int* iterations;
 
 	double x = 1.0;
 	for(int i = 0; i < 8; ++i){
 		double sum = powerSeries(x, epsilon, iterations);
-		printf("f(%.1f) = %e and the number of terms used was %d\n", x, sum, *iterations);
+		printf("f(%.1f) = %e and the number of terms used was %d\n", x, sum, &iterations);
 		x *= 2.0;
 	}
 }
 
-double powerSeries(double x, const double epsilon, int* iterations){
+double powerSeries(double x, const double epsilon, int& iterations){
 	double sum = 0.0;
 	double tn = 1.0;
-	*iterations = 0;
+	iterations = 0;
 	while(tn >= sum * epsilon){
 		sum += tn;
-		++*iterations;
-		tn = tn * 2.0 * x /(*iterations * *iterations);
+		++iterations;
+		tn = tn * 2.0 * x /(iterations * iterations);
 	}
 	return sum;
 }
